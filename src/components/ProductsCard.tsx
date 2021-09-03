790f3b6ad7927884eb905600646e041193ffc62c
jhun1211 <lvhungbk@gmail.com>
2021-09-03 16:37:04 +0700

    update show list items

--- src/components/ProductsCard.tsx
+++ src/components/ProductsCard.tsx
@@ -1,11 +1,61 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import { IProduct } from "../model/Products";
 
+const useStyles = makeStyles({
+  oot: {
+    maxWidth: 345,
+  },
+  images: {
+    height: 250,
+  },
+  content:{
+    fontSize: 16,
+  },
+  action:{
+    float: "right",
+    padding: 10
+  }
+});
 export default function ProductsCard(props: Props) {
-  return <div>
-      <p>{props.products.name}</p>
-  </div>;
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          className={classes.images}
+          image={props.products.images}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.products.name}
+          </Typography>
+          <Typography className={classes.content} color="secondary" gutterBottom variant="h6" component="h2">
+            {props.products.price} VND
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.products.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.action}>
+        <Button size="medium" variant="outlined" color="secondary">
+          Buy now
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
-interface Props{
-  products:IProduct
+interface Props {
+  products: IProduct;
 }
--- src/containers/ProductsPage.tsx
+++ src/containers/ProductsPage.tsx
@@ -10,10 +10,9 @@ import ProductsCard from "../components/ProductsCard";
 import { IProduct } from "../model/Products";
 
 // Make style with MUI
-const useStyle = makeStyles({
+const useStyles = makeStyles({
   root: {
-    marginTop: 20,
-    paddingTop: 100
+    marginTop: 100,
   },
   lazy: {
     width: "100%",
@@ -25,7 +24,7 @@ const useStyle = makeStyles({
 
 export default function ProductsPage() {
   // MUI styles
-  const classes = useStyle();
+  const classes = useStyles();
 
   // Manage state
   const [products, setProducts] = useState<IProduct[]>([]);
@@ -38,7 +37,6 @@ export default function ProductsPage() {
       .get("http://localhost:3001/api/products")
       .then((res) => {
         setProducts(res.data.data);
-        console.log(res.data.data)
         setLoadding(false);
       })
       .catch((err) => {
@@ -55,7 +53,7 @@ export default function ProductsPage() {
   return (
     // Container chua products
     <Container>
-      <Grid container spacing={2} className={classes.root}>
+      <Grid container spacing={3} className={classes.root}>
         {loading ? (
           <div className={classes.lazy}>
             <CircularProgress size="5px" thickness={5} />
